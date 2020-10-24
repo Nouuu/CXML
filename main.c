d68bd9c25e3f7b10fade1cb6e1e0f0aec37b249d
Nouu <noelarrieulacoste@yahoo.fr>
2020-10-24 15:59:37 +0200

    init xmlFile opening

--- main.c
+++ main.c
@@ -7,11 +7,36 @@ char *logFile = "log.txt";
 
 void initLogFile();
 
+FILE *openXMLFile(int argc, char **argv);
+
 int main(int argc, char **argv) {
     initLogFile();
+    FILE *xmlFile = openXMLFile(argc, argv);
 
-
+    fclose(xmlFile);
     return 0;
+}
+
+FILE *openXMLFile(int argc, char **argv) {
+    FILE *fp = NULL;
+    char message[255];
+    if (argc == 2) {
+        sprintf(message, "Open XML File : %s...", argv[1]);
+        logIt(message);
+
+        fp = fopen(argv[1], "r");
+
+        if (fp == NULL) {
+            logIt("File not found !");
+            exit(1);
+        } else {
+            logIt("File opened");
+        }
+    } else {
+        logIt("Wrong arg number");
+        exit(1);
+    }
+    return fp;
 }
 
 void initLogFile() {
