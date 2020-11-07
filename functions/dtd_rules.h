7210065ca8517086d81220b925fd730ec20b0413
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-07 18:06:40 +0100

    last

--- functions/dtd_rules.h
+++ functions/dtd_rules.h
@@ -20,6 +20,8 @@ xml_attribute *get_node_attribute(const char *attribute_name, xml_node *xmlNode)
 
 int node_contain_required_attribute(const char *required, xml_node *xmlNode);
 
+int node_contain_optional_attribute(const char *required, xml_node *xmlNode);
+
 int attribute_contain_required_value(xml_attribute *attribute, const char **str_list, int size);
 
 //e*
@@ -34,6 +36,6 @@ int node_contain_only_one_child_optional(xml_node *node, const char *name);
 //e+
 int node_contain_only_children_required(xml_node *node, const char *name);
 
-
+int check_node_child_position(xml_node *node, const char *name, int position);
 
 #endif //CXML_DTD_RULES_H
