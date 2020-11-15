af82fc132e832f123b6f86d36820492ca902220f
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-15 13:43:34 +0100

    create dtd validation branch

---
+++ functions/dtd_validation.c
@@ -0,0 +1,5 @@
+//
+// Created by Unknow on 15/11/2020.
+//
+
+#include "dtd_validation.h"
---
+++ functions/dtd_validation.h
@@ -0,0 +1,13 @@
+//
+// Created by Unknow on 15/11/2020.
+//
+
+#ifndef CXML_DTD_VALIDATION_H
+#define CXML_DTD_VALIDATION_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+#endif //CXML_DTD_VALIDATION_H
