b5663bc261c54d0e0065f79722b69a1d9c1259b9
Nouu <noelarrieulacoste@yahoo.fr>
2020-11-15 14:18:55 +0100

    root node validation

--- functions/dtd_validation.c
+++ functions/dtd_validation.c
@@ -5,6 +5,7 @@
 #include "dtd_validation.h"
 
 int validate_dtd(const char *xml_path, const char *dtd_path) {
+    char message[500] = {0};
 
     xml_document *xmlDocument = calloc(1, sizeof(xml_document));
     if (!xml_document_load(xmlDocument, xml_path)) {
@@ -16,7 +17,12 @@ int validate_dtd(const char *xml_path, const char *dtd_path) {
         return FALSE;
     }
 
-
+    //Root node assertion
+    if (strcmp(dtdDocument->root_node, xmlDocument->root_node->tag) != 0) {
+        sprintf(message, "DTD Rule error - root node don't match. Expected '%s', got '%s'",
+                dtdDocument->root_node, xmlDocument->root_node->tag);
+        return FALSE;
+    }
 
     return TRUE;
 }
